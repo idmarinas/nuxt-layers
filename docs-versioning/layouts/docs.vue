89f6b97d87ab40f352578199b652652078adf52b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-15 16:20:21 +0200

    Initial commit

---
+++ docs-versioning/layouts/docs.vue
@@ -0,0 +1,15 @@
+<template>
+  <UContainer>
+    <UPage>
+      <template #left>
+        <UPageAside>
+          <DocsAsideLeftTop />
+
+          <AppHeaderBody />
+        </UPageAside>
+      </template>
+
+      <slot />
+    </UPage>
+  </UContainer>
+</template>
