8a615fb5a47aa4026b8e07e2479cc6a6ffadc408
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-15 16:14:49 +0200

    Initial commit

---
+++ docs-versioning/.playground/nuxt.config.ts
@@ -0,0 +1,24 @@
+import {fileURLToPath} from 'node:url'
+
+export default defineNuxtConfig({
+  extends: ['docus', '..'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
+  eslint: {
+    config: {
+      // Use the generated ESLint config for lint root project as well
+      rootDir: fileURLToPath(new URL('..', import.meta.url))
+    }
+  },
+  devtools: {enable: true},
+  docsVersioning: {enable: true},
+  i18n: {
+    defaultLocale: 'en',
+    locales: [{
+      code: 'en',
+      name: 'English',
+    }, {
+      code: 'fr',
+      name: 'Français',
+    }],
+  },
+})
