c7c523e6689d3933bc58f0e753451d56cfc60146
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-15 17:15:31 +0200

    fix(nuxt): remove i18n strategy configuration

--- docs-versioning/nuxt.config.ts
+++ docs-versioning/nuxt.config.ts
@@ -6,9 +6,6 @@ const {resolve} = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
   modules: [resolve('./module')],
-  i18n: {
-    strategy: 'no_prefix'
-  },
   compatibilityDate: '2025-08-11',
   devtools: {enabled: true},
   docsVersioning: {
