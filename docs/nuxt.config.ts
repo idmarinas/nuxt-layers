9ded08c1ac72815b5b174d5ccc9cf313d14cb1f3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-11 12:59:35 +0100

    refactor(docs): migrate nuxt.config.ts from docs-versioning to docs with updated structure and compatibility date

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 11/12/2025, 12:59
+ *
+ * @project Nuxt Layers
+ * @see https://github.com/idmarinas/nuxt-layers
+ *
+ * @file nuxt.config.ts
+ * @date 09/12/2025
+ * @time 19:43
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['docus'],
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true}
+})
