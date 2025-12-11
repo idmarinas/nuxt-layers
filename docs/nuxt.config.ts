3aa1f23096951e7e577ebcf1faa9682e1601f9f2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-11 20:51:48 +0100

    refactor(docs): update nuxt.config.ts with meta information and resolver for assets

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 11/12/2025, 12:59
+ * Last modified by "IDMarinas" on 11/12/2025, 15:00
  *
  * @project Nuxt Layers
  * @see https://github.com/idmarinas/nuxt-layers
@@ -15,10 +15,15 @@
  *
  * @since 1.0.0
  */
+import {createResolver} from '@nuxt/kit'
 
+const {resolve} = createResolver(import.meta.url)
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['docus'],
+  $meta: {
+    name: 'docs-versioning'
+  },
+  css: [resolve('./app/assets/css/main.css')],
   compatibilityDate: '2025-07-15',
-  devtools: {enabled: true}
+  devtools: {enabled: true},
 })
