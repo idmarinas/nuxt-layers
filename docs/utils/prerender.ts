fa22cec1d91192edc0d29864fc1d760e2552ecfa
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-12 18:35:01 +0100

    feat(docs-utils): add addPrerenderPath utility for handling prerender paths in headers

---
+++ docs/utils/prerender.ts
@@ -0,0 +1,13 @@
+/** Extracted from @nuxt/content */
+export const addPrerenderPath = (path: string) => {
+  const event = useRequestEvent()
+  if (event) {
+    event.node.res.setHeader(
+      'x-nitro-prerender',
+      [
+        event.node.res.getHeader('x-nitro-prerender'),
+        path,
+      ].filter(Boolean).join(','),
+    )
+  }
+}
