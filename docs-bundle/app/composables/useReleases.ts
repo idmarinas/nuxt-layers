b5dccd1c2f527ad1967a08a19a56b6921527b5d8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-17 19:42:29 +0100

    refactor(docs-bundle): update useReleases composable to align with renamed collection and type adjustments

--- docs-bundle/app/composables/useReleases.ts
+++ docs-bundle/app/composables/useReleases.ts
@@ -1,17 +1,16 @@
-import type { Collections, ChangelogVCollectionItem } from '@nuxt/content'
+import type { Collections, VersionsCollectionItem } from '@nuxt/content'
 
 export const useReleases = async () => {
   const { locale, isEnabled } = useDocusI18n()
 
-  const collectionName = computed(() => isEnabled.value ? `changelog_v_${locale.value}` : 'changelog_v')
+  const collectionName = computed(() => isEnabled.value ? `versions_${locale.value}` : 'versions')
 
   const { data: lastRelease } = await useAsyncData('last_release', () => queryCollection(collectionName.value as keyof Collections)
     .order('date' as any, 'DESC')
     .where('date', 'IS NOT NULL')
     .where('date', 'NOT LIKE', '%y%')
-    .first() as Promise<ChangelogVCollectionItem>
+    .first() as Promise<VersionsCollectionItem>
   )
 
   return { lastRelease }
 }
-
