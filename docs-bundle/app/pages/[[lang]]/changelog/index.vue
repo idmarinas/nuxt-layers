5064fdab82cac6b2de420ee7e415581afd2fbeaa
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-17 19:47:12 +0100

    refactor(docs-bundle): adjust changelog collections, paths, and types for versioning consistency

--- docs-bundle/app/pages/[[lang]]/changelog/index.vue
+++ docs-bundle/app/pages/[[lang]]/changelog/index.vue
@@ -1,46 +1,45 @@
-<script setup lang="ts">
-import type {
-  Collections, ChangelogVCollectionItem, ChangelogCollectionItem, PageCollections
-} from '@nuxt/content'
+<script lang="ts" setup>
+import type { BranchsCollectionItem, Collections, PageCollections, VersionsCollectionItem } from '@nuxt/content'
 
 definePageMeta({
   layout: 'changelog',
-  path: '/:lang?/changelog/:v(\\d+\.x)?',
+  path: '/:lang?/changelog/:v(\\d+_x)?',
 })
 
 const route = useRoute()
 const { locale, isEnabled, t } = useDocusI18n()
 const appConfig = useAppConfig()
+const majorVersions = appConfig.docsBundle.majorVersions as Record<string, string>
 
 // Dynamic collection name based on i18n status
 const collectionName = computed(() => {
-  const collection = route.params.v ? 'changelog_v' : 'changelog'
+  const collection = route.params.v ? 'versions' : 'branches'
   return isEnabled.value ? `${collection}_${locale.value}` : collection
 })
-const pageName = computed(() => isEnabled.value ? `changelog_${locale.value}` : 'changelog')
+const pageName = computed(() => isEnabled.value ? `branches_${locale.value}` : 'branches')
 
 const [{ data: pages }, { data: page }, { data: surround }] = await Promise.all([
   useAsyncData(collectionName.value + route.params.v, () => {
     const query = queryCollection(collectionName.value as keyof PageCollections)
 
     if (route.params.v) {
-      query.where('branch', '=', route.params.v).order('branch' as any, 'DESC')
+      query.where('branch', '=', (route.params.v as string).replaceAll('_', '.')).order('branch' as any, 'DESC')
     } else {
       query.where('branch', 'IS NOT NULL').order('branch' as any, 'DESC')
     }
 
-    return query.all() as Promise<ChangelogVCollectionItem[]>
+    return query.all() as Promise<VersionsCollectionItem[]>
   }),
   useAsyncData(`page_${pageName.value}_${route.params.v}`, () => {
     const query = queryCollection(pageName.value as keyof Collections)
 
     if (route.params.v) {
-      query.where('branch', '=', route.params.v)
+      query.where('branch', '=', (route.params.v as string).replaceAll('_', '.'))
     } else {
       query.where('branch', 'IS NULL')
     }
 
-    return query.first() as Promise<ChangelogCollectionItem>
+    return query.first() as Promise<BranchsCollectionItem>
   }),
   useAsyncData(`page_${pageName.value}_${route.params.v}-surround`, () => {
     return queryCollectionItemSurroundings(pageName.value as keyof Collections, route.path, {
@@ -83,25 +82,29 @@ const items = useBreadcrumbItems({
   <UPage v-if="page">
     <UPageHeader v-bind="page">
       <template #headline>
-        <UBadge variant="soft" label="Changelog" />
+        <UBadge label="Changelog" variant="soft" />
       </template>
     </UPageHeader>
     <UPageBody>
       <UBreadcrumb :items="items" />
 
       <UChangelogVersions>
-        <UChangelogVersion v-for="(version, index) in pages" :key="index" v-bind="version" :to="version.path" />
+        <UChangelogVersion v-for="(version, index) in pages" :key="index" v-bind="version"
+          :date="majorVersions[version.branch.replace('.x', '')]" :to="version.path" />
       </UChangelogVersions>
 
       <UContentSurround :surround="surround" />
     </UPageBody>
     <template #right>
-      <UContentToc highlight :title="appConfig.toc?.title || t('docs.toc', '', {})" :links="page.body?.toc?.links">
+      <UContentToc :links="page.body?.toc?.links" :title="appConfig.toc?.title || t('docs.toc', '', {})" highlight>
         <template #bottom>
           <LastRelease :separator="!!page?.body?.toc?.links?.length" />
           <DocsAsideRightBottom />
         </template>
       </UContentToc>
     </template>
+  </UPage>
+  <UPage v-else>
+    No se encuentra la rama
   </UPage>
 </template>
