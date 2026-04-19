33a3b66da980599e1e1c281f7f0cdb5a7cf1b912
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-04-19 19:04:42 +0200

    refactor(docs-bundle): update `nuxt.config.ts` to include Vue devtools dependencies for optimized builds

--- docs-bundle/.playground/nuxt.config.ts
+++ docs-bundle/.playground/nuxt.config.ts
@@ -1,7 +1,15 @@
 export default defineNuxtConfig({
   extends: ['..', 'docus'],
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   docsBundle: {
     // package_name: 'idmarinas/template-bundle',
+  },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
   }
 })
