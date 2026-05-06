32bcadcb683ed9d172bcc6c01bae6d12f318ae9a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-06 15:01:02 +0200

    refactor(docs-bundle): adjust imports and format exports for consistency

--- docs-bundle/types.d.ts
+++ docs-bundle/types.d.ts
@@ -1,4 +1,4 @@
-import type {DocsBundleConfig} from './bundle.config'
+import type { DocsBundleConfig } from './interfaces'
 
 declare module '@nuxt/schema' {
   interface AppConfig {
@@ -6,4 +6,4 @@ declare module '@nuxt/schema' {
   }
 }
 
-export {}
+export { }
