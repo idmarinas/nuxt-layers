9140299b91bce6de5baceb2ad0fc04ff925a1389
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-06 19:19:27 +0200

    refactor(docs-bundle): add DocsAsideLeftBody component and remove DocsAsideLeftTop component

---
+++ docs-bundle/app/components/docs/DocsAsideLeftBody.vue
@@ -0,0 +1,24 @@
+<script
+  setup
+  lang="ts"
+>
+const { sidebarNavigation } = useSubNavigation()
+const route = useRoute()
+
+const contentNavVariants = useUIConfig('contentNavigation')
+const show = computed(() => {
+  if (route.params.lang === 'about' || route.params.lang === 'security') {
+    return false
+  } else if (route.params.slug === 'about' || route.params.lang === 'security') {
+    return false
+  }
+
+  return true
+})
+</script>
+
+<template>
+  <UContentNavigation v-if="show" :highlight="contentNavVariants.highlight ?? true"
+    :highlight-color="contentNavVariants.highlightColor" :variant="contentNavVariants.variant ?? 'link'"
+    :color="contentNavVariants.color" :navigation="sidebarNavigation" />
+</template>
---
+++ docs-bundle/app/layouts/docs.vue
@@ -0,0 +1,19 @@
+<template>
+  <UMain>
+    <UContainer>
+      <UPage>
+        <template #left>
+          <UPageAside>
+            <DocsAsideLeftTop />
+
+            <UContentSearchButton :collapsed="false" variant="soft" :ui="{ leadingIcon: 'size-4 mr-1' }" class="w-full" />
+            <USeparator class="my-4" type="dashed" />
+
+            <DocsAsideLeftBody />
+          </UPageAside>
+        </template>
+        <slot />
+      </UPage>
+    </UContainer>
+  </UMain>
+</template>
