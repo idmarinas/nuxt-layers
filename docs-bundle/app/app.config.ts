bab69e7ec1d67a307d00097ee5148720eec2b971
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-06 15:01:35 +0200

    refactor(docs-bundle): update app config to include neutral color and navigation settings

--- docs-bundle/app/app.config.ts
+++ docs-bundle/app/app.config.ts
@@ -1,6 +1,10 @@
 export default defineAppConfig({
   ui: {
-    colors: {primary: 'blue', secondary: 'cyan'}
+    colors: { primary: 'blue', secondary: 'cyan', neutral: 'zinc' }
   },
-  github: {rootDir: 'docs'},
+  github: { rootDir: 'docs' },
+  navigation: {
+    sub: 'aside', // header or 'aside'
+  },
 })
+
