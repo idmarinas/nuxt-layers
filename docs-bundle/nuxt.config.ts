8d965c5c8df254bd303acbc906c8114437ae4a49
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-07 13:20:41 +0200

    refactor(docs-bundle): remove unused hooks and resolver imports in nuxt config

--- docs-bundle/nuxt.config.ts
+++ docs-bundle/nuxt.config.ts
@@ -1,6 +1,3 @@
-import type { PageMeta } from 'nuxt/app'
-import { createResolver } from 'nuxt/kit'
-
 export default defineNuxtConfig({
   modules: ['nuxt-seo-utils'],
   mdc: {
@@ -8,17 +5,4 @@ export default defineNuxtConfig({
       langs: ['php']
     }
   },
-  hooks: {
-    'pages:resolved'(pages: PageMeta[]) {
-      const { resolve } = createResolver(import.meta.url)
-      const exclude = ['index', 'lang-index']
-      const landingTemplate = resolve('./app/templates/landing.vue')
-
-      pages.map(page => {
-        if (exclude.includes(page.name!) && (page.file as string).endsWith('docus/app/templates/landing.vue')) {
-          page.file = landingTemplate
-        }
-      })
-    }
-  }
 })
