e0567f1f4314816d9c4415674d4096a6791096a8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-07 13:19:38 +0200

    feat(docs-bundle): add Landing component with dynamic props and SVG illustration

--- docs-bundle/app/components/OgImage/Landing.takumi.vue
+++ docs-bundle/app/components/OgImage/Landing.takumi.vue
@@ -1,6 +1,9 @@
-<script lang="ts" setup>
+<script
+  lang="ts"
+  setup
+>
 
-const {title, description, headline, socials = {icons: [], username: ''}} = defineProps<{
+const { title, description, headline, socials = { icons: [], username: '' } } = defineProps<{
   title?: string,
   description?: string,
   headline?: string,
@@ -16,19 +19,14 @@ const bundle = useAppConfig().docsBundle
 <template>
   <div class="flex flex-col justify-center w-full h-full bg-blue-950 p-[100px]">
     <svg class="absolute right-0 top-0 fill-blue-200" height="593" viewBox="0 0 629 593" width="629"
-         xmlns="http://www.w3.org/2000/svg">
+      xmlns="http://www.w3.org/2000/svg">
       <g filter="url(#filter0_f_199_94966)">
         <path class="fill-blue-200"
-              d="M628.5 -578L639.334 -94.4223L806.598 -548.281L659.827 -87.387L965.396 -462.344L676.925 -74.0787L1087.69 -329.501L688.776 -55.9396L1160.22 -164.149L694.095 -34.9354L1175.13 15.7948L692.306 -13.3422L1130.8 190.83L683.602 6.50012L1032.04 341.989L668.927 22.4412L889.557 452.891L649.872 32.7537L718.78 511.519L628.5 36.32L538.22 511.519L607.128 32.7537L367.443 452.891L588.073 22.4412L224.955 341.989L573.398 6.50012L126.198 190.83L564.694 -13.3422L81.8734 15.7948L562.905 -34.9354L96.7839 -164.149L568.224 -55.9396L169.314 -329.501L580.075 -74.0787L291.604 -462.344L597.173 -87.387L450.402 -548.281L617.666 -94.4223L628.5 -578Z" />
+          d="M628.5 -578L639.334 -94.4223L806.598 -548.281L659.827 -87.387L965.396 -462.344L676.925 -74.0787L1087.69 -329.501L688.776 -55.9396L1160.22 -164.149L694.095 -34.9354L1175.13 15.7948L692.306 -13.3422L1130.8 190.83L683.602 6.50012L1032.04 341.989L668.927 22.4412L889.557 452.891L649.872 32.7537L718.78 511.519L628.5 36.32L538.22 511.519L607.128 32.7537L367.443 452.891L588.073 22.4412L224.955 341.989L573.398 6.50012L126.198 190.83L564.694 -13.3422L81.8734 15.7948L562.905 -34.9354L96.7839 -164.149L568.224 -55.9396L169.314 -329.501L580.075 -74.0787L291.604 -462.344L597.173 -87.387L450.402 -548.281L617.666 -94.4223L628.5 -578Z" />
       </g>
       <defs>
-        <filter id="filter0_f_199_94966"
-                color-interpolation-filters="sRGB"
-                filterUnits="userSpaceOnUse"
-                height="1251.52"
-                width="1255.25"
-                x="0.873535"
-                y="-659">
+        <filter id="filter0_f_199_94966" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"
+          height="1251.52" width="1255.25" x="0.873535" y="-659">
           <feFlood flood-opacity="0" result="BackgroundImageFix" />
           <feBlend in="SourceGraphic" in2="BackgroundImageFix" mode="normal" result="shape" />
           <feGaussianBlur result="effect1_foregroundBlur_199_94966" stdDeviation="40.5" />
@@ -42,11 +40,11 @@ const bundle = useAppConfig().docsBundle
           {{ headline }}
         </p>
         <h1 v-if="title" class="w-[600px] m-0 text-[75px] font-semibold mb-4 text-white"
-            style="display: block; line-clamp: 2; text-overflow: ellipsis;">
+          style="display: block; line-clamp: 2; text-overflow: ellipsis;">
           {{ title.slice(0, 60) }}
         </h1>
         <p v-if="description" class="text-[32px] text-[#E4E4E7] leading-tight"
-           style="display: block; line-clamp: 3; text-overflow: ellipsis;">
+          style="display: block; line-clamp: 3; text-overflow: ellipsis;">
           {{ description.slice(0, 200) }}
         </p>
       </div>
@@ -64,7 +62,7 @@ const bundle = useAppConfig().docsBundle
 
     <div v-if="socials.username || !socials.icons.length" class="flex flex-row justify-end items-center gap-4">
       <p v-if="socials.username" class="flex-initial text-white text-[24px] font-bold">@{{ socials.username }}</p>
-      <UIcon v-for="(icon, key) in socials.icons" :key="key" :name="icon" class="w-7 h-7 text-white" mode="svg" />
+      <UIcon v-for="(icon, key) in socials.icons" :key="key" :name="icon" class="size-7 mx-1 text-white" mode="svg" />
     </div>
   </div>
 </template>
