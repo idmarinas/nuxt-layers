a7ad443aae7d913adace8c16a5f5e3816a83e7f1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-07 13:21:13 +0200

    feat(docs-bundle): add socials configuration with platform links in nuxt config

--- docs-bundle/.playground/nuxt.config.ts
+++ docs-bundle/.playground/nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   devtools: {enabled: true},
   docsBundle: {
     // package_name: 'idmarinas/template-bundle',
+    socials: {
+      x: 'https://x.com/idmarinas',
+      reddit: 'https://reddit.com/u/idmarinas',
+      paypal: 'https://www.paypal.me/idmarinas',
+      bitly: 'https://bit.ly/m/idmarinas',
+      githubsponsors: 'https://github.com/sponsors/idmarinas',
+      linkedin: 'https://linkedin.com/in/idmarinas',
+    },
   },
   vite: {
     optimizeDeps: {
