9b36e7f96120698019d321f1c814f988ad932439
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-07 17:36:40 +0200

    feat(docs-bundle): add `libraries` configuration to nuxt config with support for Symfony and other components

--- docs-bundle/.playground/nuxt.config.ts
+++ docs-bundle/.playground/nuxt.config.ts
@@ -11,6 +11,20 @@ export default defineNuxtConfig({
       githubsponsors: 'https://github.com/sponsors/idmarinas',
       linkedin: 'https://linkedin.com/in/idmarinas',
     },
+    libraries: [
+      {
+        title: 'Symfony Components',
+        icon: 'i-tabler-brand-symfony',
+        to: 'https://www.symfony.com/components',
+        description: 'This project relies on these components for most of its features.'
+      },
+      {
+        title: 'Other component',
+        icon: 'i-tabler-components',
+        to: 'https://www.example.com',
+        description: 'Other Component used in this project.'
+      }
+    ],
   },
   vite: {
     optimizeDeps: {
