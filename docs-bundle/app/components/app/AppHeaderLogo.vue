1a186a1c0b6c5318464319ac058d95a5d9aee92a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-11 16:11:06 +0200

    feat(docs-bundle): add `AppHeaderLogo` and `Favicon` components for branding and UI enhancement

---
+++ docs-bundle/app/components/app/AppHeaderLogo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const bundle = useAppConfig().docsBundle
+</script>
+
+<template>
+  <div class="flex items-center gap-2 shrink-0">
+    <SvgFavicon class="size-6" />
+    {{ bundle.short_name }}
+  </div>
+</template>
