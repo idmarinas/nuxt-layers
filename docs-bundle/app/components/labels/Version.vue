75c7e4a219522fe5b8ac7be563b004fe739a43cf
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-12 19:59:20 +0200

    feat(docs-bundle): add `Version` component to display version details with tooltip and badge

---
+++ docs-bundle/app/components/labels/Version.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import type { BranchesCollectionItem, Collections } from '@nuxt/content'
+
+const props = defineProps<{
+  version: string
+}>()
+
+const { locale, isEnabled } = useDocusI18n()
+const collectionName = computed(() => isEnabled.value ? `branches_${locale.value}` : 'branches')
+const versionMajor = computed(() => {
+  const versionMatch = props.version.match(/^(\d+)(?:_|\.)(\d+)(?:(?:_|\.)(\d+))?$/)
+  if (!versionMatch) {
+    return 0
+  }
+
+  const [, majorText] = versionMatch
+
+  return Number(majorText)
+})
+
+const { data: branch } = await useAsyncData(`label-version-${versionMajor.value}`, async () => {
+  return await queryCollection(collectionName.value as keyof Collections)
+    .where('branch', '=', `${versionMajor.value}.x`)
+    .select('branch' as any, 'security', 'requirements')
+    .first() as BranchesCollectionItem
+})
+
+const color = computed(() => {
+  const supported = branch.value?.requirements.support
+  const security = branch.value?.security
+
+  if ('features' === supported) {
+    return 'primary'
+  } else if ('none' === supported && !security) {
+    return 'error'
+  } else if ('none' === supported && security) {
+    return 'yellow'
+  } else if ('bugs' === supported) {
+    return 'orange'
+  }
+
+  return 'neutral'
+})
+
+const variant = computed(() => {
+  const supported = branch.value?.requirements.support
+  const security = branch.value?.security
+
+  if ('features' === supported) {
+    return 'outline'
+  } else if ('none' === supported && !security) {
+    return 'solid'
+  } else if ('none' === supported && security) {
+    return 'subtle'
+  } else if ('bugs' === supported) {
+    return 'outline'
+  }
+
+  return 'solid'
+})
+
+const tooltip = computed(() => {
+  const supported = branch.value?.requirements.support
+  const security = branch.value?.security
+
+  if ('features' === supported) {
+    return 'New features bug and security fixes'
+  } else if ('none' === supported && !security) {
+    return 'No longer maintained'
+  } else if ('none' === supported && security) {
+    return 'Security fixes only'
+  } else if ('bugs' === supported) {
+    return 'Bug and security fixes'
+  }
+
+  return 'Unknown version'
+})
+</script>
+
+<template>
+  <UTooltip :text="tooltip">
+    <UBadge :color="color" :variant="variant" :icon="!branch ? '' : 'i-tabler-tag'" :label="version" />
+  </UTooltip>
+</template>
