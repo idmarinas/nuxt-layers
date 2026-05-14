d0d1aa652ca8691657e396454984db04396224d6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-14 12:18:59 +0200

    refactor(docs-bundle): remove unused ogImage and Nitro prerender configurations

--- docs-bundle/nuxt.config.ts
+++ docs-bundle/nuxt.config.ts
@@ -5,12 +5,4 @@ export default defineNuxtConfig({
       langs: ['php']
     }
   },
-  ogImage: {
-    zeroRuntime: true,  // elimina todo el código de generación en runtime
-  },
-  nitro: {
-    prerender: {
-      crawlLinks: true, // que rastree y pre-renderice todas las páginas
-    }
-  }
 })
