e9864dad603dd1c16d1b4d9042294cc4e680df5e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-09-15 14:14:24 +0200

    fix(AppHeaderCenter.vue): update active link conditions for navigation items

--- docs-bundle/app/components/app/AppHeaderCenter.vue
+++ docs-bundle/app/components/app/AppHeaderCenter.vue
@@ -6,25 +6,25 @@ const links = computed(() => [
     label: 'Documentation',
     icon: 'i-tabler-book',
     to: '/getting-started/introduction',
-    active: !route.path.startsWith('/changelog') && !route.path.startsWith('/about') && !route.path.startsWith('/security') && route.path.length > 1,
+    active: !route.path.includes('/changelog') && !route.path.endsWith('/about') && !route.path.endsWith('/security') && route.path.length > 1,
   },
   {
     label: 'Changelog',
     icon: 'i-tabler-layout',
     to: '/changelog',
-    active: route.path.startsWith('/changelog'),
+    active: route.path.includes('/changelog'),
   },
   {
     label: 'About',
     icon: 'i-tabler-user',
     href: '/about',
-    active: route.path.startsWith('/about'),
+    active: route.path.endsWith('/about'),
   },
   {
     label: 'Security Policy',
     icon: 'i-tabler-shield-check',
     href: '/security',
-    active: route.path.startsWith('/security'),
+    active: route.path.endsWith('/security'),
   }
 ])
 </script>
